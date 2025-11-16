2a9f015a999f4230040c32d27a9a58c162452abe
lbl <2523890936@qq.com>
2025-11-16 17:40:35 +0800

    bugfix

--- src/pages/front-end/message/index.tsx
+++ src/pages/front-end/message/index.tsx
@@ -1,5 +1,11 @@
+import { Camera, Heart, Search, User,UmbrellaOff } from 'lucide-react';
 const Message = () => (
     <div>
+        <Camera size={24} />
+        <Heart className="text-red-500" />
+        <Search className="text-blue-500" />
+        <User size={20} strokeWidth={1.5} />
+        <UmbrellaOff/>
         我是消息 组件
     </div>
 )
