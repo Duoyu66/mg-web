c6f1bf6c3b380421010a73c0253771937e86e15f
LBL <2523890936@qq.com>
2025-12-08 13:41:15 +0800

    bnugfix

--- src/pages/front-end/question/hooks/reqAddLvExp.ts
+++ src/pages/front-end/question/hooks/reqAddLvExp.ts
@@ -2,7 +2,7 @@ import requestClient from "@/utils/requestClient";
 
 export const reqAddLvExp = (data: any) => {
     return requestClient.request({
-        url: '/api/lv/addExp',
+        url: '/api/lv/addUserExp',
         method: 'post',
         headers: {'content-type': 'application/json'},
         data
